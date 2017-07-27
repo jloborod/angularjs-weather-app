a6fe9472e06f05bc35e23805aef7dc1fd346ba12
jloborod <jloborod@gmail.com>
2017-07-27 16:11:18 +0100

    added tests for common module

---
+++ src/app/common/app.spec.js
@@ -0,0 +1,30 @@
+describe('App', function() {
+  beforeEach(module('ui.router'));
+
+  beforeEach(module('common'));
+
+  beforeEach(module(function ($stateProvider) {
+    $stateProvider.state('weather', { url: '/weather' })
+  }));
+
+  describe('Routes', function () {
+    var $state, $location, $rootScope;
+
+    function goTo(url) {
+      $location.url(url);
+      $rootScope.$digest();
+    }
+
+    beforeEach(inject(function ($injector) {
+      $state = $injector.get('$state');
+      $location = $injector.get('$location');
+      $rootScope = $injector.get('$rootScope');
+    }));
+
+    it('should redirect to weather state', function() {
+      goTo('/app');
+
+      expect($state.current.name).toEqual('weather');
+    });
+  });
+});
